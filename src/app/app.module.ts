d8094046185d63e096149501ce385d38a156f2e8
Zenwalker <zenwalker1985@gmail.com>
2018-10-23 11:35:32 +0530

    added image, deleted unwanted files, updated reference fr landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ConvertToSpacesPipe } from './shared/pipes/convertToSpace-pipe';
 import { StarComponent } from './shared/star/star.component';
 import { CommentsToProductsComponent } from './shared/comments-to-products/comments-to-products.component';
 import { HttpClientModule } from '@angular/common/http';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     ConvertToSpacesPipe,    
-    StarComponent, CommentsToProductsComponent
+    StarComponent, CommentsToProductsComponent    
   ],
   imports: [
     BrowserModule,
@@ -21,6 +21,6 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [WelcomeComponent]
 })
 export class AppModule { }
