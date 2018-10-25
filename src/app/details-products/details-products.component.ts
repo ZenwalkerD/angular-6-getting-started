2e472c49ccd3203e5a59cde2aafedbd79bf23d86
Zenwalker <zenwalker1985@gmail.com>
2018-10-25 16:13:11 +0530

    Changes

--- src/app/details-products/details-products.component.ts
+++ src/app/details-products/details-products.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../product-list/product';
 import {ActivatedRoute} from '@angular/router';
 import { ProductDataService } from '../product-list/services/product-data.service';
-import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   templateUrl: './details-products.component.html',
@@ -13,18 +12,20 @@ export class DetailsProductsComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute, private productService: ProductDataService) { }
 
   products: IProduct[];
+
   productId : string;
   errorMessage : any;
-  productInfo : IProduct;
+  selectedProduct : IProduct;
 
   ngOnInit() {
     this.productId = this.activatedRoute.snapshot.paramMap.get('id');
-    this.productService.getProductsData().subscribe(item => {
+     this.productService.getProductsData().subscribe(item => {
       this.products= item,
-      this.products.forEach(x =>
-        console.log("Matching??? " + this.productId == x.productCode))      
-    }, 
-      error => this.errorMessage = <any> error);    
+      this.selectedProduct = this.products.find(x => this.productId === x.productCode)
+      console.log(this.selectedProduct.productCode);
+    },  
+      error => this.errorMessage = <any> error);      
   }
+  
 
 }
